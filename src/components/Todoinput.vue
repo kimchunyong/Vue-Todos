641eea5aba8c06ab76b5192def7d02d2bd279ee2
Kimchunyong <lovemy5854@naver.com>
2018-06-27 18:48:44 +0900

    new todo

---
+++ src/components/Todoinput.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" name="" id="" v-model="newTodoItem" v-on:keyup.enter="addTodo" placeholder="할 일목록을 입력해 주세요.">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+        </span>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodoItem: ''
+        }
+    },
+    methods: {
+        addTodo() {
+            if(this.newTodoItem !== "") {
+                var value = this.newTodoItem && this.newTodoItem.trim();
+                localStorage.setItem(value, value);
+                this.clearInput();
+            }
+        },
+        clearInput(){
+            this.newTodoItem ='';
+        }
+    }
+}
+</script>
+
+<style>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: #fff;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: inline-block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: #fff;
+  vertical-align: middle;
+}
+</style>

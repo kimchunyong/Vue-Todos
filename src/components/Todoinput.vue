18b4b11d2bcdf0be66ce895314966ad3b20c7f43
Kimchunyong <lovemy5854@naver.com>
2018-07-10 11:58:47 +0900

    git add todo

--- src/components/Todoinput.vue
+++ src/components/Todoinput.vue
@@ -5,28 +5,42 @@
             <i class="addBtn fa fa-plus" aria-hidden="true"></i>
         </span>
         
+        <modal v-if="showModal" @close="showModal=false">
+            <h3 slot="header">경고</h3>
+            <span slot="footer" @click="showModal=false">
+                할 일을 입력하세요.
+                <i class="closeModalBtn fa fa-times" aria-hidden="true"></i>
+            </span>
+        </modal>
     </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
 export default {
-    data() {
-        return {
-            newTodoItem: ''
-        }
-    },
-    methods: {
-        addTodo() {
-            if(this.newTodoItem !== "") {
-                var value = this.newTodoItem && this.newTodoItem.trim();
-                this.$emit('addTodo', value);
-                this.clearInput();
-            }
-        },
-        clearInput(){
-            this.newTodoItem ='';
-        }
+  data() {
+    return {
+      newTodoItem: '',
+      showModal: false
     }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+		this.$emit('addTodo', value)
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  },
+  components: {
+    Modal: Modal
+  }
 }
 </script>
 
